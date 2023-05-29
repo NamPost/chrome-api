6b1ee529393fb6a2489c099d12774ea946801b66
kudasav <savanhu.k@yahoo.com>
2023-05-29 10:57:08 +0200

    added support for postgreSQL databases

--- config/config.js
+++ config/config.js
@@ -1,22 +1,17 @@
-import dotenv from "dotenv"
+import dotenv, { config } from "dotenv"
 
 dotenv.config()
 
 const REJECT_UNAUTHORIZED = process.env.DATABASE_USE_SSL == "true"
 
-export default {
+const dbConfig = {
 	"development": {
 		"username": process.env.DATABASE_USER,
 		"password": process.env.DATABASE_PASSWORD,
 		"database": process.env.DATABASE_NAME,
 		"host": process.env.DATABASE_HOST,
 		"port": process.env.DATABASE_PORT,
-		"dialect": "mysql",
-		"dialectOptions": {
-			"ssl": {
-				"rejectUnauthorized": REJECT_UNAUTHORIZED,
-			},
-		}
+		"dialect": process.env.DATABASE_TYPE
 	},
 	"test": {
 		"username": process.env.DATABASE_USER,
@@ -24,12 +19,7 @@ export default {
 		"database": process.env.DATABASE_NAME,
 		"host": process.env.DATABASE_HOST,
 		"port": process.env.DATABASE_PORT,
-		"dialect": "mysql",
-		"dialectOptions": {
-			"ssl": {
-				"rejectUnauthorized": REJECT_UNAUTHORIZED,
-			},
-		}
+		"dialect": process.env.DATABASE_TYPE
 	},
 	"production": {
 		"username": process.env.DATABASE_USER,
@@ -37,11 +27,23 @@ export default {
 		"database": process.env.DATABASE_NAME,
 		"host": process.env.DATABASE_HOST,
 		"port": process.env.DATABASE_PORT,
-		"dialect": "mysql",
-		"dialectOptions": {
-			"ssl": {
-				"rejectUnauthorized": REJECT_UNAUTHORIZED,
-			},
-		}
+		"dialect": process.env.DATABASE_TYPE
 	}
 };
+
+let ssl_options = {
+	"ssl": {
+		"rejectUnauthorized": REJECT_UNAUTHORIZED,
+	},
+}
+
+if (REJECT_UNAUTHORIZED) {
+	dbConfig.development.dialectOptions = ssl_options
+	dbConfig.test.dialectOptions = ssl_options
+	dbConfig.production.dialectOptions = ssl_options
+}
+
+
+console.log(dbConfig)
+
+export default dbConfig;
--- models/index.js
+++ models/index.js
@@ -4,11 +4,28 @@ import apiKeys from "./apiKeys.js";
 import jobs from "./jobs.js";
 import dotenv from "dotenv";
 import mysql2 from "mysql2";
-
+import pg from "pg";
 
 dotenv.config()
 
 const REJECT_UNAUTHORIZED = process.env.DATABASE_USE_SSL == "true"
+let dialectModule = undefined
+let dialectOptions = {}
+
+if (process.env.DATABASE_TYPE == "mysql"){
+	dialectModule = mysql2
+}
+
+if (process.env.DATABASE_TYPE == "postgres"){
+	dialectModule = pg
+}
+
+// set the ssl options
+if(REJECT_UNAUTHORIZED){
+	dialectOptions.ssl = {
+		rejectUnauthorized: REJECT_UNAUTHORIZED,        
+	}
+}
 
 const sequelize = new Sequelize(
 	process.env.DATABASE_NAME,
@@ -18,15 +35,11 @@ const sequelize = new Sequelize(
 		host: process.env.DATABASE_HOST,
 		port: process.env.DATABASE_PORT,
 		dialect: process.env.DATABASE_TYPE,
-		dialectModule: mysql2,
+		dialectModule: pg,
 		operatorsAliases: false,
 		pool: process.env.pool,
 		logging: false,
-		dialectOptions: {
-			ssl: {
-				rejectUnauthorized: REJECT_UNAUTHORIZED,        
-			}
-		}
+		dialectOptions: dialectOptions
 	}
 );
 
